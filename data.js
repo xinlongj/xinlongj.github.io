3610dc32160e10b60e57b52948b676afea6a7df9
zhanghao <308507814@qq.com>
2021-05-21 16:13:05 +0800

    添加日历插件

---
+++ data.js
@@ -0,0 +1,47 @@
+const data = [
+  {
+    groupId: 999,
+    title: '北京计算所开会',
+    start: '2021-05-01T10:00:00'
+  },
+  {
+    title: 'Long Event',
+    start: '2021-05-07',
+    end: '2021-05-10'
+  },
+  {
+    groupId: 999,
+    title: '北京计算所开会',
+    start: '2021-05-09T16:00:00'
+  },
+  {
+    groupId: 999,
+    title: '北京计算所开会',
+    start: '2021-05-16T16:00:00'
+  },
+ 
+  {
+    title: '北京计算所开会',
+    start: '2021-05-12T10:30:00',
+    end: '2021-05-12T12:30:00'
+  },
+  
+  {
+    title: 'Happy Hour',
+    start: '2021-05-12T17:30:00'
+  },
+  {
+    title: '北京计算所开会',
+    start: '2021-05-12T20:00:00'
+  },
+  {
+    title: 'Birthday Party',
+    start: '2021-05-13T07:00:00'
+  },
+  {
+    title: 'Click for Google',
+    url: 'http://google.com/',
+    start: '2021-05-28'
+  }
+]
+export default data
