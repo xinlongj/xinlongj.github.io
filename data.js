60421ae2efe4265a39e37485f59585953f107661
xinlongj <jiangxinlong2009@gmail.com>
2021-05-28 17:51:30 +0800

    略

--- data.js
+++ data.js
@@ -1,57 +1,53 @@
 const data = [
-  {
-    groupId: 999,
-    title: '北京计算所开会',
-    start: '2021-02-01T10:00:00'
-  },
-  {
-    groupId: 999,
-    title: '北京计算所开会',
-    start: '2021-05-01T10:00:00'
-  },
-  {
-    title: 'Long Event',
-    start: '2021-05-07',
-    end: '2021-05-10'
-  },
-  {
-    groupId: 999,
-    title: '北京计算所开会',
-    start: '2021-05-09T16:00:00'
-  },
-  {
-    groupId: 999,
-    title: '北京计算所开会',
-    start: '2021-05-11T16:00:00'
-  },
-  {
-    groupId: 999,
-    title: '北京计算所开会',
-    start: '2021-05-16T16:00:00'
-  },
-
-  {
-    title: '北京计算所开会',
-    start: '2021-05-12T10:30:00',
-    end: '2021-05-12T12:30:00'
-  },
-  
-  {
-    title: 'Happy Hour',
-    start: '2021-05-12T17:30:00'
-  },
-  {
-    title: '北京计算所开会',
-    start: '2021-05-12T20:00:00'
-  },
-  {
-    title: 'Birthday Party',
-    start: '2021-05-13T07:00:00'
-  },
-  {
-    title: 'Click for Google',
-    url: 'http://google.com/',
-    start: '2021-05-28'
-  }
+    // May of 2021
+    {
+        title: '课题组例会',
+        start: '2021-05-31T9:30:00'
+        end: '2021-05-31T10:30:00'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-01T10:00:00'
+    },
+    {
+        title: 'Long Event',
+        start: '2021-05-07',
+        end: '2021-05-10'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-09T16:00:00'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-11T16:00:00'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-16T16:00:00'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-12T10:30:00',
+        end: '2021-05-12T12:30:00'
+    },
+    // June of 2021
+    {
+        title: 'Happy Hour',
+        start: '2021-05-12T17:30:00'
+    },
+    {
+        title: '北京计算所开会',
+        start: '2021-05-12T20:00:00'
+    },
+    {
+        title: 'Birthday Party',
+        start: '2021-05-13T07:00:00'
+    },
+    {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2021-05-28'
+    }
 ]
 export default data
