17228b942dc3b63a7ca38244be8cf858352b6d45
xinlongj <jiangxinlong2009@gmail.com>
2021-05-31 09:47:03 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,32 @@
 const data = [
     // May of 2021
     {
-        title: '课题组例会',
-        start: '2021-05-31T9:30:00',
-        end: '2021-05-31T10:30:00'
-    },
-    {
-        title: 'Still available now',
+        title: 'Available',
         start: '2021-05-31T13:30:00',
         end: '2021-05-31T16:00:00'
     },
     // June of 2021
     {
-        title: 'Still available now',
+        title: 'Available',
         start: '2021-06-02T13:30:00',
         end: '2021-06-02T18:00:00'
     },
     {
-        title: '北京计算所开会',
-        start: '2021-05-12T20:00:00'
+        title: 'Available',
+        start: '2021-06-03T13:30:00',
+        end: '2021-06-03T17:30:00'
     },
     {
-        title: 'Birthday Party',
-        start: '2021-05-13T07:00:00'
+        title: 'Available',
+        start: '2021-06-04T9:30:00',
+        end: '2021-06-04T17:30:00'
     },
+    /* Example
     {
         title: 'Click for Google',
         url: 'http://google.com/',
         start: '2021-05-28'
     }
+    */
 ]
 export default data
