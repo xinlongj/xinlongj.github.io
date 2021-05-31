ceb075557b06a11d8f3eab74d27e513a1a482d27
xinlongj <jiangxinlong2009@gmail.com>
2021-05-31 10:27:55 +0800

    Create data.js

--- data.js
+++ data.js
@@ -1,10 +1,15 @@
 const data = [
     // May of 2021
     {
-        title: 'Available',
+        title: '1V1 Chen Qian',
         start: '2021-05-31T13:30:00',
-        end: '2021-05-31T16:00:00'
+        end: '2021-05-31T14:30:00'
     },
+    {
+        title: '1V1 Jinteng',
+        start: '2021-05-31T14:40:00',
+        end: '2021-05-31T15:40:00'
+    },    
     // June of 2021
     {
         title: 'Available',
@@ -17,8 +22,13 @@ const data = [
         end: '2021-06-03T17:30:00'
     },
     {
-        title: 'Available',
+        title: '1v1 Chenlong',
         start: '2021-06-04T09:30:00',
+        end: '2021-06-04T10:30:00'
+    },
+    {
+        title: 'Available',
+        start: '2021-06-04T10:30:00',
         end: '2021-06-04T17:30:00'
     },
     {
